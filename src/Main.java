a072929e4bd3a36297001f1132bdb287f2d98f1c
Nurislam <maksatbekkov@icloud.com>
2022-12-07 18:38:19 +0600

    "Added classes and some actions"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        mainPrint();
+        start();
+    }
+    public static void start(){
+
+    }
+
+    public static void mainPrint(){
+        System.out.println("Choose your move\n" +
+                "(1) ROCK\n" +
+                "(2) PAPER\n" +
+                "(3) SCISSORS");
     }
 }
---
+++ src/Turn.java
@@ -0,0 +1,3 @@
+public enum Turn {
+    ROCK, PAPER, SCISSORS
+}
